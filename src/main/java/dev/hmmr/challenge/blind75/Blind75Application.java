b562d9d452a5e18109ab9bd92586b847c656abba
Max <github@hmmr.dev>
2024-07-14 12:01:39 +0200

    Initial commit

---
+++ src/main/java/dev/hmmr/challenge/blind75/Blind75Application.java
@@ -0,0 +1,12 @@
+package dev.hmmr.challenge.blind75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blind75Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Blind75Application.class, args);
+  }
+}
