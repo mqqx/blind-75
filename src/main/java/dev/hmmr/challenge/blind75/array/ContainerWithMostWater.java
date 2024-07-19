1e8bbfe729576c185939abbb602c1aa93203a77d
Max <github@hmmr.dev>
2024-07-19 13:51:45 +0200

    add solution for Container With Most Water

---
+++ src/main/java/dev/hmmr/challenge/blind75/array/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package dev.hmmr.challenge.blind75.array;
+
+public class ContainerWithMostWater {
+  public int maxArea(int[] height) {
+    int right = height.length - 1;
+    int left = 0;
+    int max = 0;
+
+    while (left < right) {
+      // use two pointers and calculate area while moving the pointer of the lower line
+      final int curMax = Math.min(height[left], height[right]) * (right - left);
+      max = Math.max(max, curMax);
+
+      if (height[left] > height[right]) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+
+    return max;
+  }
+}
