b9379d06abf3e2e26354e270c299150e64a4a494
Max <github@hmmr.dev>
2024-08-03 10:53:17 +0200

    add solution for 128. Longest Consecutive Sequence

--- src/main/java/dev/hmmr/challenge/blind75/graph/LongestConsecutiveSequence.java
+++ src/main/java/dev/hmmr/challenge/blind75/graph/LongestConsecutiveSequence.java
@@ -4,22 +4,26 @@ import java.util.Arrays;
 
 public class LongestConsecutiveSequence {
   public int longestConsecutive(int[] nums) {
+    // sort array to make it easy to count longest consecutive sequence while iterating
     Arrays.sort(nums);
 
     int longestSeqCount = 0;
     int seqCount = 0;
     int last = Integer.MIN_VALUE;
+
     for (int cur : nums) {
       last++;
 
+      // for a consecutive sequence last++ needs to be equal to cur
       if (cur == last) {
         seqCount++;
+        // otherwise the sequence can be reset, as we have a sorted array
       } else if (cur > last) {
         seqCount = 1;
       }
 
-      longestSeqCount = Math.max(longestSeqCount, seqCount);
       last = cur;
+      longestSeqCount = Math.max(longestSeqCount, seqCount);
     }
 
     return longestSeqCount;
