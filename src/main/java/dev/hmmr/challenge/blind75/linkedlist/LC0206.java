a8e797739b130db1a48cafead8705659b7b4b724
Max <github@hmmr.dev>
2024-08-04 14:34:44 +0200

    add solution for 206. Reverse Linked List

---
+++ src/main/java/dev/hmmr/challenge/blind75/linkedlist/LC0206.java
@@ -0,0 +1,57 @@
+package dev.hmmr.challenge.blind75.linkedlist;
+
+/** Definition for singly-linked list. */
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+public class LC0206 {
+  public ListNode reverseListIteratively(ListNode head) {
+    // will be the new head of the reversed list
+    ListNode last = null;
+    // current node which will be processed
+    ListNode cur = head;
+
+    while (cur != null) {
+      // store the next node
+      ListNode next = cur.next;
+      // reverse the link
+      cur.next = last;
+      // move last one step forward
+      last = cur;
+      // move current to the next node
+      cur = next;
+    }
+    return last;
+  }
+
+  // recursively
+  public ListNode reverseList(ListNode head) {
+    // if the list is empty or has only one node, it's already reversed
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    // recursively reverse the rest of the list
+    ListNode result = reverseList(head.next);
+    // point the node after head back to head
+    head.next.next = head;
+    // set head's next to null to avoid cycles and finish the reversal
+    head.next = null;
+
+    // return the new head of the reversed list
+    return result;
+  }
+}
